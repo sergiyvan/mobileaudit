276ab325a20dbc7398e7c90b0d33a27959c443e4
Sergey <serg@zedat.fu-berlin.de>
2013-12-26 19:18:20 +0100

    fix migration

--- db/migrate/20131226114415_register_operators.rb
+++ db/migrate/20131226114415_register_operators.rb
@@ -1,12 +1,12 @@
 class RegisterOperators < ActiveRecord::Migration
   def self.up
     User.create(email: 'oper1@checklines.pro', role: "operator", password: '123qwe123', password_confirmation: '123qwe123')
-    User.create(email: 'oper1@checklines.pro', role: "operator", password: '123qwe123', password_confirmation: '123qwe123')
-    User.create(email: 'oper1@checklines.pro', role: "operator", password: '123qwe123', password_confirmation: '123qwe123')
+    User.create(email: 'oper2@checklines.pro', role: "operator", password: '123qwe123', password_confirmation: '123qwe123')
+    User.create(email: 'oper3@checklines.pro', role: "operator", password: '123qwe123', password_confirmation: '123qwe123')
   end
   def self.down
     User.find_by_email('oper1@checklines.pro').delete
-    User.find_by_email('oper1@checklines.pro').delete
-    User.find_by_email('oper1@checklines.pro').delete
+    User.find_by_email('oper2@checklines.pro').delete
+    User.find_by_email('oper3@checklines.pro').delete
   end
 end

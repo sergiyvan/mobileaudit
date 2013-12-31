088e406f9fd90ccf297f11d1f1254a609f8f933b
Sergey <serg@zedat.fu-berlin.de>
2013-12-31 11:12:01 +0100

    task view with nice content

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -3,7 +3,7 @@
 function taskStepNumerize(){
   var domSteps = document.getElementById('task_steps').getElementsByClassName('task_step'), key = -1, max = domSteps.length, domNumer;
   while(++key<max){
-    domNumer = domSteps[key].getElementsByClassName('task_numer')[0];
+    domNumer = domSteps[key].getElementsByClassName('task_step_numer')[0];
     domNumer.innerHTML = domNumer.innerHTML.replace( RegExp('\\d+$'), key+1 );
   }
   var domStepsCount = document.getElementById('task_steps').getElementsByClassName('task_steps_count')[0];
@@ -13,13 +13,13 @@ function taskStepNumerize(){
 /// скрывает поле предопределенных ответов, если тип задачи изменен на другой
 function taskTypeTurn( dom ){
   var domStep = dom.parentNode.parentNode;
-  var domAnswerS = domStep.getElementsByClassName('task_answers')[0];
+  var domAnswerS = domStep.getElementsByClassName('task_step_answers')[0];
   domAnswerS.style.display=dom.selectedIndex===0?'block':'none';
 };
 
 function taskStepAnswerPut( dom ){
   var domStep = dom.parentNode.parentNode;
-  var domAnswerS = domStep.getElementsByClassName('task_answers')[0];
+  var domAnswerS = domStep.getElementsByClassName('task_step_answers')[0];
   var domAnswerNew = document.createElement('div');
   domAnswerNew.innerHTML = '<input type="text" name="task_step[][answer][]" /><input type="button" onclick="taskStepAnswerCut(this)" value="Удалить данный вариант ответа" />';
   domAnswerS.insertBefore( domAnswerNew, dom );
@@ -34,7 +34,7 @@ function taskStepAnswerCut( dom ){
 function taskStepPut(dom){
   var mold = '\
                 <input class="task_step_cut" type="button" onclick="taskStepCut(this)" value="Удалить данную подзадачу" />\
-                <div class="task_numer">Номер шага: 0</div>\
+                <div class="task_step_numer">Номер шага: 0</div>\
                 <div>Тип подзадачи:\
                   <select required="required" name="task_step[][type]" onchange="taskTypeTurn(this)">\
                     <option value="0" selected="selected">Предопределенные ответы</option>\
@@ -46,7 +46,7 @@ function taskStepPut(dom){
                 <div>Описание подзадачи/вопрос:\
                   <textarea name="task_step[][question]"></textarea>\
                 </div>\
-                <div class="task_answers">Предопределенные ответы:<br />\
+                <div class="task_step_answers">Предопределенные ответы:<br />\
                   <input type="button" onclick="taskStepAnswerPut(this)" value="Добавить вариант ответа" />\
                 </div>\
   ';

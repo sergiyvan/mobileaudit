0e05a0717cc3d1f8479c3f0da83de323ca907da0
serg <serg@serg-president.(none)>
2014-01-20 23:06:53 +0100

    add validation for expdate

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,7 @@ class Task < ActiveRecord::Base
 	has_many :task_instances, dependent: :destroy
 	has_many :users, :through => :task_instances
 	serialize :content
+	validates :expdate, presence: true
 
 	geocoded_by :address
 	after_validation :geocode
